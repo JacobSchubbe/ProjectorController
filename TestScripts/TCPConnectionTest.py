2eb966694d302ecce23a53ea5da239c40e826534
Jacob Schubbe <jacob.schubbe@mogroup.com>
2025-01-03 11:14:00 +0200

    organized files

--- TestScripts/TCPConnectionTest.py
+++ TestScripts/TCPConnectionTest.py
@@ -1,17 +1,18 @@
 import socket
 from time import sleep
 
-# communication = bytes([0x45, 0x53, 0x43, 0x2F, 0x56, 0x50, 0x2E, 0x6E, 0x65, 0x74, 0x10, 0x03, 0x00, 0x00, 0x00, 0x00]) # ESC/VP.net
-# queryState = bytes([0x50, 0x57, 0x52, 0x3F, 0x0D]) # PWR?
-# powerOff = bytes([0x50, 0x57, 0x52, 0x20, 0x4F, 0x46, 0x46, 0x5C, 0x72, 0x5C, 0x6E]) # PWROFF
-# naturalColorMode = bytes([0x43, 0x4D, 0x4F, 0x44, 0x45, 0x20, 0x30, 0x37, 0x0D]) # CMODE 07
-communication = "ESC/VP.net\x10\x03\x00\x00\x00\x00"
-naturalColorMode = "CMODE 07"
+
+
+# =========== SYSTEM CONTROL ========================
+startCommunication = "ESC/VP.net\x10\x03\x00\x00\x00\x00"
 
 powerQuery = "PWR?"
 powerOff = "PWR OFF"
 powerOn = "PWR ON"
 
+volumeMuteOn = "MUTE ON" # only with non-ARC sound
+volumeMuteOff = "MUTE OFF" # only with non-ARC sound
+volumeMuteQuery = "MUTE?"
 volumeUp = "VOL INC" # only with non-ARC sound
 volumeDown = "VOL DEC" # only with non-ARC sound
 volumeQuery = "VOL?"
@@ -20,13 +21,38 @@ sourceHDMI1 = "SOURCE 30"
 sourceHDMI2 = "SOURCE A0"
 sourceHDMI3 = "SOURCE C0"
 sourceHDMILan = "SOURCE 53"
+sourceListQuery = "SOURCELIST?"
+
+# ============ SYSTEM INFORMATION ===================
+projectorNameQuery = "NWPNAME?"
+serialNumberQuery = "SNO?"
+errorQuery = "ERR?"
+lampHoursQuery = "LAMP?"
+operationalTimeQuery = "ONTIME?"
+signalStatusQuery = "SIGNAL?" # 00: No signal, 01: Signal detected (2D), 02: Signal detected (3D), FF: Unsupported signal
+
+# ================ IMAGE ===================
+naturalColorMode = "CMODE 07"
+
+imageReverseHorizontalOn = "HREVERSE ON"
+imageReverseHorizontalOff = "HREVERSE OFF"
+imageReverseHorizontalQuery = "HREVERSE?"
+imageReverseVerticalOn = "VREVERSE ON"
+imageReverseVerticalOff = "VREVERSE OFF"
+imageReverseVerticalQuery = "VREVERSE?"
+
 
 # same for CONTRAST, DENSITY, TINT
 brightnessUp = "BRIGHT INC"
 brightnessDown = "BRIGHT DEC"
 brightnessQuery = "BRIGHT?"
 
+statusLEDIlluminationOn = "ILLUM 01"
+statusLEDIlluminationOff = "ILLUM 00"
+statusLEDIlluminationQuery = "ILLUM?"
 
+
+# ================== KEYS ===================
 
 keyPower = "KEY 01"
 keyMenu = "KEY 03"
@@ -53,10 +79,6 @@ keyForward = "KEY D7"
 keyMute = "KEY D8"
 keyLinkMenu = "KEY D9"
 
-projectorNameQuery = "NWPNAME?"
-lampHoursQuery = "LAMP?"
-errorQuery = "ERR?"
-sourceListQuery = "SOURCELIST?"
 
 powerOnStatus = b'PWR=03\r:'
 powerBootingStatus = b'PWR=02\r:'
@@ -64,7 +86,7 @@ powerOffStatus = b'PWR=01\r:'
 errorResponse = b'Err\r:'
 
 def send_command(s:socket, str):
-    if str != communication:
+    if str != startCommunication:
         str = f'{str}\r'
     bytes = str.encode('ascii')
     s.sendall(bytes)
@@ -93,10 +115,8 @@ def main():
             s.connect((host, port))
             print(f"Connected to {host}:{port}.")
 
-            send_command(s, communication)
-            send_command(s, volumeQuery)
-            send_command(s, volumeUp)
-            send_command(s, volumeQuery)
+            send_command(s, startCommunication)
+            send_command(s, serialNumberQuery)
 
     except socket.error as e:
         print(f"Error with the TCP connection: {e}")
