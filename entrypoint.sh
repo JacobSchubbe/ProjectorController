ce1c3b5356b2ef7c0e5bdb630b25dd2621c0ebcd
Jacob Schubbe <jacob.schubbe@mogroup.com>
2025-01-21 19:10:28 +0200

    creating the lirc inside the docker container instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# Start Lirc
+lircd --device=/dev/lirc0 --output=/var/run/lirc/lircd
+
 # Start ASP.NET Core backend
 dotnet /app/ProjectController.dll &
 
 # Start Nginx
 nginx -g "daemon off;"
+
